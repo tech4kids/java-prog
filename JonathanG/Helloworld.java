c8910312f4b6a1703a362768833ed0fcd7e88e4b
jonathan-314 <jonguo6@gmail.com>
2015-05-01 17:43:58 -0700

    Update Helloworld.java

--- JonathanG/Helloworld.java
+++ JonathanG/Helloworld.java
@@ -1,9 +1,9 @@
-public class Helloworld          
-
-{  
-	public static void main (String[] args)
-
-           {
-            System.out.println("Hello World! Hi!"); 
-           }
- }
+public class FirstClass {
+	public static void main(String[] args) {
+System.out.println("Hello World! Hi");
+int x = 9;
+int y = 10;
+int result = x + y;
+System.out.println("result = " + result);
+}
+}
