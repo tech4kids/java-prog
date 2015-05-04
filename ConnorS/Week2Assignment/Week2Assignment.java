383757d9ce4b33fc9626b0f90efbc144ec6d9bb2
CShu12 <connor.shu@gmail.com>
2015-05-03 17:12:30 -0700

    Week 2  assignment

---
+++ ConnorS/Week2Assignment/Week2Assignment.java
@@ -0,0 +1,15 @@
+package week.pkg2.assignment;
+
+
+public class Week2Assignment {
+
+    public static void main(String[] args) {
+       int x;
+       int y;
+       x = 10;
+       y = 20;
+       System.out.println(x);
+       System.out.println(y);
+    }
+    
+}
