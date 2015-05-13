a1e6a38856d8adec1959cdbb35617eb53d505907
Kickball2015 <marshalx08@gmail.com>
2015-05-12 21:33:34 -0700

    Up to date remove array input element!

--- MarshalX/BONUS.java
+++ MarshalX/BONUS.java
@@ -6,19 +6,24 @@ public class BONUS {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int y = 5;
+		int j;
 		int maximum = 100;
 		int [] Array;
 		Array = new int[5];
 		for(int i = 0; i < 5; i++){
 			Array[i] = (int)(Math.random()*maximum);
+			//Array[i] = i;
 			System.out.println(Array[i]);
 		}
 		System.out.println("Input # to remove");
 		int x = input.nextInt();
 		for(int i = 0; i < y; i++){
 			if(Array[i] == x){
-				for(int j = i; j < y-1; j++){
-					Array[j] = Array[j+1];
+				j = i;
+				while(Array[j] == x){
+					for(j = i; j < y-1; j++){
+						Array[j] = Array[j+1];
+					}
 				}
 				y = y-1;
 			} 
