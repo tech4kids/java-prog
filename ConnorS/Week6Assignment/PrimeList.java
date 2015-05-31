6e40c08e83c5ee331b087da8e4ed9bb9761e6c6a
CShu12 <connor.shu@gmail.com>
2015-05-30 22:12:39 -0700

    Added percentage

--- ConnorS/Week6Assignment/PrimeList.java
+++ ConnorS/Week6Assignment/PrimeList.java
@@ -7,10 +7,14 @@ public class PrimeList {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter an upperbound: ");
         int upperBound = in.nextInt();
+        int counter = 0;
         for(int i = 2; i <= upperBound; i++){
             if(isPrime(i) == true){
+                counter++;
                 System.out.println(i + " ");
             }
         }
+        double percentage = counter*100/upperBound;
+        System.out.println("There are " + counter + " primes less than or equal to your upper bound. That's " + percentage + "%");
     }
 }
